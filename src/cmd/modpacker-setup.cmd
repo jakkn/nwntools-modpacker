74d418747ea95b227a92adba5290b296350e13a3
pspeed <pspeed>
2004-08-08 06:41:11 +0000

    Added an additional command line entry point. Modified the modpacker setup file to generate scripts for running the different XML conversions.

--- src/cmd/modpacker-setup.cmd
+++ src/cmd/modpacker-setup.cmd
@@ -2,6 +2,12 @@
 @java -cp nwn-tools.jar org.progeeks.util.PathTool @java -cp @cwd@\nwn-tools.jar org.progeeks.nwn.ModPacker %%1 %%2 > ModPacker.cmd
 @echo Creating ModUnpacker script.
 @java -cp nwn-tools.jar org.progeeks.util.PathTool @java -cp @cwd@\nwn-tools.jar org.progeeks.nwn.ModReader %%1 %%2 > ModUnpacker.cmd
+@echo Creating ModToXml script.
+@java -cp nwn-tools.jar org.progeeks.util.PathTool @java -cp @cwd@\nwn-tools.jar org.progeeks.nwn.ModToXml %%1 %%2 > ModToXml.cmd
+@echo Creating GffToXml script.
+@java -cp nwn-tools.jar org.progeeks.util.PathTool @java -cp @cwd@\nwn-tools.jar org.progeeks.nwn.GffToXml %%1 %%2 > GffToXml.cmd
+@echo Creating XmlToGff script.
+@java -cp nwn-tools.jar org.progeeks.util.PathTool @java -cp @cwd@\nwn-tools.jar org.progeeks.nwn.XmlToGff %%1 %%2 > XmlToGff.cmd
 @echo Creating setpath script.
 @java -cp nwn-tools.jar org.progeeks.util.PathTool set PATH=%%PATH%%;@cwd@ > setpath.cmd
 @echo Done.
---
+++ src/java/org/progeeks/nwn/ModToXml.java
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2004, Paul Speed
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1) Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2) Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3) Neither the names "Progeeks", "Meta-JB", nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.progeeks.nwn;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *  Converts the files from a module into
+ *  individual XML representations.  This is just a convenience
+ *  wrapper for the GffToXml tool.
+ *
+ *  @version   $Revision$
+ *  @author    Paul Speed
+ */
+public class ModToXml
+{
+    public static void main( String[] args ) throws Exception
+    {
+        if( args.length < 2 )
+            {
+            System.out.println( "Usage: ModToXml <module file> <destionation dir>" );
+            System.out.println();
+            System.out.println( "Where: <mod file> is the module file to unpack and convert." );
+            System.out.println( "    and <destination directory> is location where the" );
+            System.out.println( "    files will be unpacked." );
+            System.out.println( "    GFF files will be converted to XML." );
+            System.out.println( "    All other files will be copied." );
+            return;
+            }
+
+        System.out.println( "--- Gff To XML version 0.1 ---" );
+
+        long start = System.currentTimeMillis();
+
+        File inFile = new File( args[0] );
+        File outDir = new File( args[1] );
+        GffToXml converter = new GffToXml( outDir );
+
+        converter.processFile( inFile );
+
+        long totalTime = System.currentTimeMillis() - start;
+        double secs = (double)totalTime / 1000.0;
+
+        System.out.println( "\nTotal time:" + secs + " seconds." );
+    }
+}
