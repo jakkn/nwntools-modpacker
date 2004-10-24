81c3a3ad688eab4df34b2f5815d1944dc41f79e0
pspeed <pspeed>
2004-10-24 06:41:33 +0000

    Fixed the script to take all parameters and pass them on to the java program.

--- src/cmd/minimap-export-setup.cmd
+++ src/cmd/minimap-export-setup.cmd
@@ -1,5 +1,5 @@
 @echo Creating MiniMapExport script.
-@java -cp nwn-tools.jar org.progeeks.util.PathTool @java -cp @cwd@\nwn-tools.jar;@cwd@\nwn-io.jar org.progeeks.nwn.MiniMapExporter %%1 %%2 > MiniMapExport.cmd
+@java -cp nwn-tools.jar org.progeeks.util.PathTool @java -cp @cwd@\nwn-tools.jar;@cwd@\nwn-io.jar org.progeeks.nwn.MiniMapExporter %%* > MiniMapExport.cmd
 @echo Creating setpath script.
 @java -cp nwn-tools.jar org.progeeks.util.PathTool set PATH=%%PATH%%;@cwd@ > setpath.cmd
 @echo Done.
