1443d00e6204eb9f1ed7adcd2a2bf381f7237ae0
pspeed <pspeed>
2005-05-15 13:55:19 +0000

    Removed the minimap-export line from this file since all of the stuff needed to run minimap-export isn't included in this distribution.

--- src/cmd/modpacker-setup.cmd
+++ src/cmd/modpacker-setup.cmd
@@ -8,8 +8,6 @@
 @java -cp nwn-tools.jar org.progeeks.util.PathTool @java -cp @cwd@\nwn-tools.jar org.progeeks.nwn.GffToXml %%1 %%2 > GffToXml.cmd
 @echo Creating XmlToGff script.
 @java -cp nwn-tools.jar org.progeeks.util.PathTool @java -cp @cwd@\nwn-tools.jar org.progeeks.nwn.XmlToGff %%1 %%2 > XmlToGff.cmd
-@echo Creating MiniMapExport script.
-@java -cp nwn-tools.jar org.progeeks.util.PathTool @java -cp @cwd@\nwn-tools.jar org.progeeks.nwn.MiniMapExporter %%1 %%2 > MiniMapExport.cmd
 @echo Creating setpath script.
 @java -cp nwn-tools.jar org.progeeks.util.PathTool set PATH=%%PATH%%;@cwd@ > setpath.cmd
 @echo Done.
