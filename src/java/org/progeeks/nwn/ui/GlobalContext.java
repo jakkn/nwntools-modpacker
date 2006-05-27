2d06fc14f64773b55019a7991f2df8baea4ab988
pspeed <pspeed>
2006-05-27 13:12:46 +0000

    Fixed to compile... the file it depended on has been removed and I'm not sure why the fix to this file was never checked in.

--- src/java/org/progeeks/nwn/ui/GlobalContext.java
+++ src/java/org/progeeks/nwn/ui/GlobalContext.java
@@ -37,6 +37,7 @@ import java.io.*;
 import java.util.*;
 
 import org.progeeks.cmd.swing.SwingCommandProcessor;
+import org.progeeks.graph.xml.*;
 import org.progeeks.meta.*;
 import org.progeeks.meta.beans.*;
 import org.progeeks.meta.format.*;
